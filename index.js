bb44f5d5d741276adc29befde7b1218f020c4ca8
Jesus Vega <jesusvega00@gmail.com>
2020-02-03 10:09:46 -0800

    Finised

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code here
+const http = require("http");
+
+const server = http.createServer(function(req, res) {
+    // Log a message when message is recieved
+    console.log(' Recieved ' + req.method + ' request for: ' + req.url);
+    // send headerss
+    res.writeHead(200, {'ContentType': 'text/plain'});
+    // send body response 
+    res.end("Hello World")
+});
+
+// listen for incoming connections 
+server.listen(8080, 'localhost', null, function (){
+    // log a msg that the server is listening and port 
+    console.log('server is listening on localhost:8080');
+});
